e75d591c7cdb0a1ee5504cc7f8f850c7a3561b02
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-14 16:44:56 +0200

    prova funzione generateNumber

--- js/script.js
+++ js/script.js
@@ -28,3 +28,49 @@ const checkWord = function(){
 
 inviaBtn.addEventListener('click', checkWord);
 
+/* 
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. 
+
+input per utente: scegliere pari o dispari;
+input2 per utente: inserire numero da 1 a 5 (num1);
+PC genera numero random (da 1 a 5) (num2);
+somma di num1 + num2;
+verificare se somma è pari o dispari;
+dichiarare vincitore; */
+
+const iniziaBtn = document.getElementById('inizia-btn');
+const container2 = document.querySelector('.container2');
+const generaBtn = document.getElementById('genera');
+const numberResult = document.createElement('div');
+container2.append(numberResult); 
+
+const checkNumber = function(){
+    numberResult.innerHTML = ''
+    const userNumber = document.getElementById('user-number').value;
+    console.log(userNumber);
+
+    if(userNumber < 1 || userNumber > 5){
+        numberResult.innerHTML = "Inserire un numero compreso tra 1 e 5";
+    } 
+    else{
+        numberResult.innerHTML = "Il tuo numero è: " + userNumber;
+    }
+}
+iniziaBtn.addEventListener('click', checkNumber);
+
+
+let min = 1;
+let max = 5; 
+
+function generateNumber(min, max){   
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(generateNumber());
+
+
+generaBtn.addEventListener('click', generateNumber);
+
